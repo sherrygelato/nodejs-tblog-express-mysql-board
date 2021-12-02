a7ed508c5642d698e60c0bb1835c45f72b204e85
sherrygelato <sherrygelato@kakao.com>
2021-12-02 14:29:34 +0900

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Node.js + Express + MySQL을 이용한 게시판 만들기
+
+- 시리즈 : 1, 2, 3, 4
+
+### 1 
+- https://thisblogbusy.tistory.com/entry/Creating-a-bulletin-board-using-NodeJS-EXPRESS-MySQL-1?category=900700 
+
+### 2
+- https://thisblogbusy.tistory.com/entry/Creating-a-bulletin-board-using-NodeJS-EXPRESS-MySQL-2?category=900700
+
+### 3
+- https://thisblogbusy.tistory.com/entry/Creating-a-bulletin-board-using-NodeJS-EXPRESS-MySQL-3?category=900700
+
+### 4
+- https://thisblogbusy.tistory.com/entry/Creating-a-bulletin-board-using-NodeJS-EXPRESS-MySQL-4?category=900700
---
+++ backend/app/config/mysql.js
@@ -0,0 +1,36 @@
+import { createConnection } from 'mysql';
+
+// mysql 연결 
+const mysqlConnection = {
+    init: function () {
+        return createConnection({
+            host: process.env.MYSQL_HOST,
+            port: process.env.MYSQL_PORT,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASS,
+            database: process.env.MYSQL_DB,
+            charset: process.env.MYSQL_CHARSET // Emoji - utf8mb4 
+        });
+    }, open: function (conn) {
+        conn.connect(function (err) {
+            if (err) {
+                console.error('MySQL connection failed.');
+                console.error('Error Code : ' + err.code);
+                console.error('Error Message : ' + err.message);
+            } else {
+                console.log('MySQL connection successful.');
+            }
+        });
+    },
+    close: function (conn) {
+        conn.end(function (err) {
+            if (err) {
+                console.error('MySQL Terminate failed.');
+                console.error('Error Code : ' + err.code);
+                console.error('Error Message : ' + err.message);
+            } else {
+                console.log("MySQL Terminate connection.");
+            }
+        });
+    }
+}; export default mysqlConnection;
